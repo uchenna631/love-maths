4f6862bf7a7816caca3e3a42de7f6d1d94937d1b
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-02-19 19:03:10 +0000

    add event listener to javaScript

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,23 @@
+// wait for the DOM to finish loading before running the game
+// Get the button element and add event listener to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function() {
+            if (this.getAttribute('data-type') === 'submit') {
+                alert('You clicked submit')
+            } else {
+                let gameType = this.getAttribute('data-type');
+                alert(`You clicked ${gameType}`);
+
+            }
+        })
+    }
+})
+
+
 function runGame() {
 
 }
@@ -29,3 +49,5 @@ function displaySubtractQuestion() {
 function displayMultiplyQuestion() {
     
 }
+
+
